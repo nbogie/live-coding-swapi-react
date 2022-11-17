50c5aa520364d1bd099e372c08e15643b397fb73
nbogie <neillbogie@googlemail.com>
2022-11-17 16:38:52 +0000

    person number chooser

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,21 +4,22 @@ import { useState } from 'react';
 
 
 function App() {
-
-  const [personString, setPersonString] = useState<string | null>(null);
+  type Person = string;
+  const [person, setPerson] = useState<Person | null>(null);
+  const [personNumber, setPersonNumber] = useState<number>(1);
 
   async function handleFetchButtonClicked() {
-    fetchAndStore()
+    fetchAndStore(personNumber)
   }
 
-  async function fetchAndStore() {
-    logForMe("starting fetch")
-    const swapiPersonURL = "https://swapi.dev/api/people/1"
+  async function fetchAndStore(personNumberToFetch: number) {
+    const swapiPersonURL = "https://swapi.dev/api/people/" + personNumberToFetch
+    logForMe("starting fetch: " + personNumberToFetch)
     const resp = await fetch(swapiPersonURL)
     const obj = await resp.json()
 
     const str = JSON.stringify(obj, null, 2)
-    setPersonString(str);
+    setPerson(str);
     logForMe(str)
   }
 
@@ -26,14 +27,33 @@ function App() {
     console.log(str);
     toast(str);
   }
+  function handlePersonNumberChange(e: any) {
+    const desiredPersonNumber = e.target.value;
+    setPersonNumber(desiredPersonNumber);
+    fetchAndStore(desiredPersonNumber)
+
+  }
 
   return (
     <div className="App">
       <h1>Star Wars People</h1>
       <button onClick={handleFetchButtonClicked}>Fetch People</button>
-      {personString ? <pre>{personString}</pre> : <p>no data yet</p>}
+      {person ? <pre>{person}</pre> : <p>no data yet</p>}
+      <p>personNumber: {personNumber}</p>
+
+      <input type="number"
+        onChange={handlePersonNumberChange}
+        value={personNumber}
+        min={1} max={6}
+      />
+
     </div>
   )
+}
+
+function pickRandom<T>(arr: T[]): T {
+  const ix = Math.floor(Math.random() * arr.length);
+  return arr[ix];
 }
 
 export default App
