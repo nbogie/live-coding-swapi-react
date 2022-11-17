79ae38ad9f07eee9957964a0aa40b30a9aac6c70
nbogie <neillbogie@googlemail.com>
2022-11-17 16:13:03 +0000

    basic fetch  on button press

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,38 @@
-import { useState } from 'react'
-import reactLogo from '../assets/react.svg'
-import './App.css'
+import './App.css';
+import { toast } from 'react-toastify';
+import { useState } from 'react';
+
 
 function App() {
 
+  const [personString, setPersonString] = useState<string | null>(null);
 
-  function handleFetchButtonClicked() {
-    console.log("handle fetch button clicked")
+  async function handleFetchButtonClicked() {
+    fetchAndStore()
   }
+
+  async function fetchAndStore() {
+    logForMe("starting fetch")
+    const swapiPersonURL = "https://swapi.dev/api/people/1"
+    const resp = await fetch(swapiPersonURL)
+    const obj = await resp.json()
+
+    const str = JSON.stringify(obj, null, 2)
+    setPersonString(str);
+    logForMe(str)
+  }
+
+  function logForMe(str: string) {
+    console.log(str);
+    toast(str);
+  }
+
   return (
     <div className="App">
-      <h1>Hello Star Wars fans or everyone else</h1>
+      <h1>Star Wars People</h1>
       <button onClick={handleFetchButtonClicked}>Fetch People</button>
+      {personString ? <pre>{personString}</pre> : <p>no data yet</p>}
     </div>
-
   )
 }
 
